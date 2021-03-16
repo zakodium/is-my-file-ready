9dc81ab86db0d5dcba2349a29d92cf041cd44daf
Xavier Stouder <xavier@stouder.io>
2021-03-16 17:30:34 +0100

    feat: write README and expose functions

---
+++ src/checks/index.ts
@@ -0,0 +1,6 @@
+import { editTime } from './editTime';
+import { endsWithBytes } from './endsWithBytes';
+import { endsWithStr } from './endsWithStr';
+import { sameSize } from './sameSize';
+
+export { editTime, endsWithBytes, endsWithStr, sameSize };
--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,4 @@ export default async function isMyFileReady(
 }
 
 export * from './types';
+export * from './checks';
